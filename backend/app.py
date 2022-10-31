c6a1512323d3c55b1268bafa842a5b00b45f06bf
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-10-31 10:21:25 -0700

    added backed

---
+++ backend/app.py
@@ -0,0 +1,37 @@
+from email import message
+from flask import Flask, jsonify, request
+from flask_jwt_extended import jwt_required, JWTManager, create_access_token, get_jwt
+
+app = Flask("__name__")
+
+app.config['SECRET_KEY'] = "FILL THIS IS LATER"
+
+JWTManager(app)
+
+users = [
+        {
+        'first':'Jacob', 
+        'last':"Lapkin",
+        'email':'jacobglapkin@gmail.com',
+        'password':'testpassword'}
+        ]   
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.get_json()
+        if 'email' not in user.keys() and 'password' not in user.keys():
+            return jsonify(message='missing data'), 400
+        email = user['email']
+        password = user['password']
+        token = create_access_token(email)
+        return jsonify(message='Successfully logged in', token=token), 200
+
+
+@app.route('/home', methods=['GET'])
+@jwt_required
+def home():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
