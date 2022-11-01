aded42dfe1de087f97052478b7483d44e7dc2316
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-11-01 09:49:46 -0700

    added content to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./../App.css";
 import JumbotronTwo from "./../jumbotron/Jumbotronabout";
+import image1 from './../images/about1.jpg'
 
 function About() {
   return (
@@ -28,8 +29,10 @@ function About() {
           </div>
           <div className="col-md-4 col-sm-12 col-lg-4 p-4">
             <div className="text-center duo-cont p-4">
-              <h5 class='text-main'>
-                Han Salo and Chubaka, Mick Jagger and Keith Richards, and now the next best duo in education. Interpersonal mixed with brilliance available at all times. 
+              <h5 class="text-main">
+                Han Salo and Chubaka, Mick Jagger and Keith Richards, and now
+                the next best duo in education. Interpersonal mixed with
+                brilliance available at all times.
               </h5>
             </div>
           </div>
@@ -43,6 +46,21 @@ function About() {
                 <h6>Bachelors, International Relations </h6>
                 <h6 className="text-secondary">University of Sydney</h6>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid p-4 mt-3">
+        <div className="page-cont">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-md-6 col-lg-6 col-sm-12">
+              <h3>Why get started</h3>
+              <p>Everyday we accept the results of the world without asking questions. Would it not be better to understand there reasons behind interest rates, or even 
+                what is causing your rent price to increase?
+              </p>
+            </div>
+            <div className="col-md-6 col-lg-6 col-sm-12">
+              <img src={image1} className='rounded shadow'/>
             </div>
           </div>
         </div>
