770ffa7b9632b67397d367b5c3cc3041809b32d9
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-11-02 09:20:13 -0700

    added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import { React, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react"; 
-import './../navbar/Navbar.css'
+import { useState } from "react";
+import "./../navbar/Navbar.css";
+import "./../App.css"
+import image1 from './../images/about1.jpg'
 
 function Homepage(props) {
   const user = props.user;
@@ -19,8 +21,50 @@ function Homepage(props) {
   return (
     <>
       <button className="logout" onClick={logout}>
-      <i class="fa-solid fa-door-open fa-2x"></i>
+        <i class="fa-solid fa-door-open fa-2x"></i>
       </button>
+      <div className="dash-cont">
+
+      <div className="row">
+        <div className="col-md-6 col-lg-6 col-sm-12">
+          <div className="row p-4">
+            <div className="shadow rounded p-4 d-flex flex-row justify-content-around">
+              <div className="d-flex flex-column justify-content-around">
+              <h2>Welcome back, {user.first}</h2>
+              <p className="text-secondary">Rating: {user.rating}/10</p>
+              </div>
+              <div>
+              <img src={image1} height="150" className="rounded"/>
+
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-lg-6 col-sm-12 p-4">
+
+            </div>
+            <div className="col-md-6 col-lg-6 col-sm-12 p-4">
+
+            </div>
+          </div>
+          <div className="row p-4">
+
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-6 col-sm-12">
+          <div className="row p-4">
+
+          </div>
+          <div className="row p-4">
+            
+          </div>
+          <div className="row p-4">
+
+          </div>
+        </div>
+      </div>
+      </div>
+
     </>
   );
 }
